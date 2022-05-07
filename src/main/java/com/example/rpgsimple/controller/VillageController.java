20d357cd2e2b0e5e33648f05f8c063573eae8893
belkvch <rachellinak@gmail.com>
2022-05-07 17:02:30 +0300

    #3 Create VillageController, create seller with buying poitons

--- src/main/java/com/example/rpgsimple/controller/VillageController.java
+++ src/main/java/com/example/rpgsimple/controller/VillageController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.*;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/village")
@@ -21,9 +23,24 @@ public class VillageController {
     public String villagePage(Model model) {
         User userCurrent = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Character characterUser = characterService.findUserByIdUser(userCurrent.getId());
-        model.addAttribute("character",characterUser);
+        model.addAttribute("character", characterUser);
         Inventory inventoryByCharacter = inventoryService.findInventoryByCharacter(characterUser.getInventoryCharacter().getId());
-        model.addAttribute("inventory",inventoryByCharacter);
+        model.addAttribute("inventory", inventoryByCharacter);
         return "village";
+    }
+
+    @PostMapping
+    public String buyPotion(@RequestParam(value = "id") Long id, Model model) {
+        Character currentCharacter = characterService.findById(id);
+        Inventory currentInventory = inventoryService.findInventoryByCharacter(currentCharacter.getInventoryCharacter().getId());
+        BigDecimal changeGold = currentInventory.getGold().subtract(new BigDecimal(500));
+        if (changeGold.compareTo(BigDecimal.valueOf(0)) > 0 || changeGold.compareTo(BigDecimal.valueOf(0)) == 0) {
+            currentInventory.setGold(changeGold);
+            currentInventory.setHpBottle(currentInventory.getHpBottle() + 1);
+            inventoryService.save(currentInventory);
+        } else {
+            model.addAttribute("GoldError", "You haven't gold");
+        }
+        return "redirect:/village";
     }
 }
